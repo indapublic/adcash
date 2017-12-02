11fccaa82a9bf68ca0f4d0964ce149d6d59088a0
Vasilii Zolotukhin <mail@indapublic.com>
2017-12-02 19:16:13 +1000

    Added orders serialisation

--- api/src/AppBundle/Entity/UserOrder.php
+++ api/src/AppBundle/Entity/UserOrder.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * UserOrder
@@ -18,6 +19,8 @@ class UserOrder
      * @ORM\Column(name="id", type="string")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="UUID")
+     *
+     * @Groups({"default"})
      */
     private $id;
 
@@ -26,6 +29,8 @@ class UserOrder
      *
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User", inversedBy="orders")
      * @ORM\JoinColumn(name="user_id", referencedColumnName="id", nullable=false)
+     *
+     * @Groups({"default"})
      */
     private $user;
 
@@ -33,7 +38,9 @@ class UserOrder
      * @var Product
      *
      * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Product", inversedBy="orders")
-     * @ORM\Column(name="product", type="object")
+     * @ORM\JoinColumn(name="product_id", referencedColumnName="id", nullable=false)
+     *
+     * @Groups({"default"})
      */
     private $product;
 
@@ -41,6 +48,8 @@ class UserOrder
      * @var int
      *
      * @ORM\Column(name="price", type="bigint")
+     *
+     * @Groups({"default"})
      */
     private $price;
 
@@ -48,6 +57,8 @@ class UserOrder
      * @var int
      *
      * @ORM\Column(name="quantity", type="integer")
+     *
+     * @Groups({"default"})
      */
     private $quantity;
 
@@ -55,6 +66,8 @@ class UserOrder
      * @var int
      *
      * @ORM\Column(name="total", type="bigint")
+     *
+     * @Groups({"default"})
      */
     private $total;
 
@@ -62,12 +75,24 @@ class UserOrder
      * @var \DateTime
      *
      * @ORM\Column(name="date_created", type="datetime")
+     *
+     * @Groups({"default"})
      */
     private $dateCreated;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_updated", type="datetime")
+     *
+     * @Groups({"default"})
+     */
+    private $dateUpdated;
 
     public function __construct()
     {
         $this->dateCreated = new \DateTime("now");
+        $this->dateUpdated = new \DateTime("now");
         $this->quantity = 0;
         $this->price = 0;
         $this->total = 0;
@@ -118,6 +143,10 @@ class UserOrder
     {
         $this->product = $product;
 
+        if ($product instanceof Product) {
+            $this->setPrice($product->getPrice());
+        }
+
         return $this;
     }
 
@@ -140,7 +169,7 @@ class UserOrder
      */
     public function setPrice(float $price)
     {
-        $this->price = (int) $price * 100;
+        $this->price = (int) ($price * 100);
 
         return $this;
     }
@@ -186,7 +215,17 @@ class UserOrder
      */
     public function calcTotal()
     {
-        $this->setTotal($this->getPrice() * $this->getQuantity() * 100);
+        $total = $this->getPrice() * $this->getQuantity();
+
+        /**
+         * Discount of 20% must be applied to the total cost of any order
+         * when at least 3 items of "Pepsi Cola" are selected. (see the wireframe)
+         */
+        if ($this->getProduct()->getDiscount() && $this->getQuantity() > 2) {
+            $total *= 0.8;
+        }
+
+        $this->setTotal($total);
 
         return $this;
     }
@@ -200,7 +239,7 @@ class UserOrder
      */
     public function setTotal(float $total)
     {
-        $this->total = (int) $total * 100;
+        $this->total = (int) ($total * 100);
 
         return $this;
     }
@@ -216,13 +255,13 @@ class UserOrder
     }
 
     /**
-     * Set dateCreated.
+     * Set date created.
      *
      * @param \DateTime $dateCreated
      *
      * @return UserOrder
      */
-    public function setDateCreated($dateCreated)
+    public function setDateCreated(\DateTime $dateCreated)
     {
         $this->dateCreated = $dateCreated;
 
@@ -230,13 +269,37 @@ class UserOrder
     }
 
     /**
-     * Get dateCreated.
+     * Get date created.
      *
-     * @return \DateTime
+     * @return string
      */
-    public function getDateCreated()
+    public function getDateCreated(): string
     {
         return $this->dateCreated->setTimezone(new \DateTimeZone('Asia/Vladivostok'))->format('Y-m-d H:i:s');
+    }
+
+    /**
+     * Set date updated.
+     *
+     * @param \DateTime $dateUpdated
+     *
+     * @return UserOrder
+     */
+    public function setDateUpdated(\DateTime $dateUpdated)
+    {
+        $this->dateUpdated = $dateUpdated;
+
+        return $this;
+    }
+
+    /**
+     * Get date updated.
+     *
+     * @return string
+     */
+    public function getDateUpdated(): string
+    {
+        return $this->dateUpdated->setTimezone(new \DateTimeZone('Asia/Vladivostok'))->format('Y-m-d H:i:s');
     }
 }
 
